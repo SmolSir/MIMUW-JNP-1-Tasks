fb3200f05cc16e0770a27e1b94531719a33b49a7
Laplasjan107 <67481570+Laplasjan107@users.noreply.github.com>
2021-12-08 10:02:09 +0100

    Dodana klasa Node

--- 5. Virus/virus_genealogy.h
+++ 5. Virus/virus_genealogy.h
@@ -2,12 +2,33 @@
 #define VIRUS_VIRUS_GENEALOGY_H
 
 #include <vector>
+#include <memory>
+#include <map>
+
+template<typename Virus>
+class Node {
+public:
+  Virus virus;
+  std::vector<std::shared_ptr<Node<Virus>>> children;
+  std::vector<typename Virus::id_type> parents;
+  
+  Node(Virus::id_type const &virus_id) : virus(virus_id) {}
+  
+  void add_parent(Virus::id_type const &parent_id) {
+    parents.push_back(parent_id);
+  }
+
+  void add_child(Node<Virus> *child_virus) {
+    std::shared_ptr<Node<Virus>> pointer_to_child(child_virus);
+    children.push_back(pointer_to_child);
+  }
+};
+
 
 template<typename Virus>
 class VirusGenealogy {
 private:
-
-
+  map<typename Virus::id_type, Node<Virus>> viral_map;
 public:
 
     constexpr VirusGenealogy(typename Virus::id_type const &stem_id);
