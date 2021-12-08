2d76e1779e16c226c182cab2d9531e61a3de41c1
Bartosz Smolarczyk <92160712+SmolSir@users.noreply.github.com>
2021-12-08 11:37:28 +0100

    Early implementation of VirusGenealogy

--- 5. Virus/virus_genealogy.h
+++ 5. Virus/virus_genealogy.h
@@ -5,55 +5,133 @@
 #include <memory>
 #include <map>
 
+class VirusNotFoundException : public std::exception {
+    virtual const char* what() const throw() {
+        return "VirusNotFound";
+    }
+};
+
+class VirusAlreadyCreatedException : public std::exception {
+    virtual const char* what() const throw() {
+        return "VirusAlreadyCreated";
+    }
+};
+
+class TriedToRemoveStemVirusException : public std::exception {
+    virtual const char* what() const throw() {
+        return "TriedToRemoveStemVirus";
+    }
+};
+
+
 template<typename Virus>
 class Node {
 public:
-  Virus virus;
-  std::vector<std::shared_ptr<Node<Virus>>> children;
-  std::vector<typename Virus::id_type> parents;
-  
-  Node(Virus::id_type const &virus_id) : virus(virus_id) {}
-  
-  void add_parent(Virus::id_type const &parent_id) {
-    parents.push_back(parent_id);
-  }
+    Virus virus;
+    std::vector<std::shared_ptr<Node<Virus>>> children;
+    std::vector<typename Virus::id_type> parents;
 
-  void add_child(Node<Virus> *child_virus) {
-    std::shared_ptr<Node<Virus>> pointer_to_child(child_virus);
-    children.push_back(pointer_to_child);
-  }
+    Node(typename Virus::id_type const &virus_id) : virus(virus_id) {}
+
+    void add_parent(typename Virus::id_type const &parent_id) {
+        parents.push_back(parent_id);
+    }
+
+    void add_child(Node<Virus> *child_virus) {
+        std::shared_ptr<Node<Virus>> pointer_to_child(child_virus);
+        children.push_back(pointer_to_child);
+    }
 };
 
 
 template<typename Virus>
 class VirusGenealogy {
 private:
-  map<typename Virus::id_type, Node<Virus>> viral_map;
+    std::map<typename Virus::id_type, Node<Virus>> viral_map;
+    typename Virus::id_type stem_id;
+
 public:
 
-    constexpr VirusGenealogy(typename Virus::id_type const &stem_id);
+    constexpr VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id(stem_id) {};
 
-    constexpr typename Virus::id_type get_stem_id() const;
+    constexpr typename Virus::id_type get_stem_id() const {
+        return stem_id;
+    };
 
+    // TODO
     // trzeba samemu zdefiniować children_iterator
     constexpr VirusGenealogy<Virus>::children_iterator get_children_begin(typename Virus::id_type const &id) const;
-
+    // TODO
     // trzeba samemu zdefiniować children_iterator
     constexpr VirusGenealogy<Virus>::children_iterator get_children_end(typename Virus::id_type const &id) const;
 
-    std::vector<typename Virus::id_type> get_parents(typename Virus::id_type const &id) const;
+    // TODO
+    std::vector<typename Virus::id_type> get_parents(typename Virus::id_type const &id) const {
+        if (!exists(id)) {
+            throw VirusNotFoundException();
+        }
+        return viral_map[id].parents; // tutaj można tak czy trzeba przez copy() ?
+    };
 
-    bool exists(typename Virus::id_type const &id) const;
+    bool exists(typename Virus::id_type const &id) const {
+        return viral_map.contains(id);
+    };
 
-    const Virus& operator[](typename Virus::id_type const &id) const;
+    const Virus& operator[](typename Virus::id_type const &id) const {
+        if (!exists(id)) {
+            throw VirusNotFoundException();
+        }
+        return &viral_map[id];
+    };
 
+    void create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id) {
+        if (exists(id)) {
+            throw VirusAlreadyCreatedException();
+        }
+        if (!exists(parent_id)) {
+            throw VirusNotFoundException();
+        }
 
-    void create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id);
-    void create(typename Virus::id_type const &id, std::vector<typename Virus::id_type> const &parent_ids);
+        viral_map[id] = Node<>(id);
+        viral_map[id].add_parent(parent_id);
+        viral_map[parent_id].add_child(&viral_map[id]);
+    };
 
-    void connect(typename Virus::id_type const &child_id, typename Virus::id_type const &parent_id);
+    void create(typename Virus::id_type const &id, std::vector<typename Virus::id_type> const &parent_ids) {
+        if (exists(id)) {
+            throw VirusAlreadyCreatedException();
+        }
+        for (auto parent : parent_ids) {
+            if (!exists(parent)) {
+                throw VirusNotFoundException();
+            }
+        }
 
-    void remove(typename Virus::id_type const &id);
+        viral_map[id] = Node<>(id);
+        for (auto parent : parent_ids) {
+            viral_map[id].add_parent(parent);
+            viral_map[parent].add_child(&viral_map[id]);
+        }
+    };
+
+    void connect(typename Virus::id_type const &child_id, typename Virus::id_type const &parent_id) {
+        if (!exists(child_id) || !exists(parent_id)) {
+            throw VirusNotFoundException();
+        }
+        viral_map[child_id].add_parent(parent_id);
+        viral_map[parent_id].add_child(&viral_map[child_id]);
+    };
+
+    // TODO
+    void remove(typename Virus::id_type const &id) {
+        if (!exists(id)) {
+            throw VirusNotFoundException();
+        }
+        if (id == get_stem_id()) {
+            throw TriedToRemoveStemVirusException();
+        }
+        // tu będzie usuwanie jak się dowiemy co ma robić
+    };
 };
 
 #endif //VIRUS_VIRUS_GENEALOGY_H
