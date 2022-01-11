f4e55e7a923a79fc93c80d05a57970739ceaba18
Katarzyna Kloc <kasiakloc00@gmail.com>
2022-01-11 18:37:35 +0100

    Add elementary classes without implementation

--- Rover/rover.h
+++ Rover/rover.h
@@ -4,8 +4,65 @@
 using coordinate_t = int;
 
 class Sensor {
-    virtual bool is_safe([[maybe_unused]] coordinate_t x,
-                         [[maybe_unused]] coordinate_t y) = 0;
+public:
+    virtual bool is_safe([[maybe_unused]] coordinate_t x, [[maybe_unused]] coordinate_t y) = 0;
+};
+
+class Direction {
+public:
+    static int WEST;
+    static int EAST;
+};
+
+
+class Operation {
+    virtual void run();
+};
+
+class move_forward : public Operation {
+public:
+    move_forward();
+};
+
+class move_backward : public Operation {
+public:
+    move_backward();
+};
+
+class rotate_right : public Operation {
+public:
+    rotate_right();
+};
+
+class rotate_left : public Operation {
+public:
+    rotate_left();
+};
+
+class compose : public Operation {
+public:
+    compose(std::initializer_list<Operation> list);
+};
+
+class Rover {
+public:
+    void execute(const std::string &s);
+
+    void land(std::pair<coordinate_t, coordinate_t>, int);
+
+    inline friend std::ostream &operator<<(std::ostream &o, const Rover &r);
+
+};
+
+class RoverBuilder {
+public:
+    RoverBuilder() = default;
+
+    RoverBuilder program_command(char, const Operation &o);
+
+    RoverBuilder add_sensor(std::unique_ptr<Sensor> s);
+
+    Rover build();
 };
 
 #endif //ROVER_ROVER_H
