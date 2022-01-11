0cf4980ceec282d1890faa0164fa9ff2919e4e00
Katarzyna Kloc <kasiakloc00@gmail.com>
2022-01-11 20:03:08 +0100

    Change some classes to enable compilation

--- Rover/rover.h
+++ Rover/rover.h
@@ -1,21 +1,26 @@
 #ifndef ROVER_ROVER_H
 #define ROVER_ROVER_H
 
+#include <vector>
+
 using coordinate_t = int;
 
-class Coordinates {
-public:
-    Coordinates() = delete;
-    Coordinates(coordinate_t x, coordinate_t y) : _x(x), _y(y) {}
+enum class Direction {WEST, EAST, SOUTH, NORTH};
 
-    friend std::ostream &operator<<(std::ostream &os, const Coordinates &c) {
-        os << "(" << c._x << ", " << c._y << ")";
+class Position {
+public:
+    Position() = delete;
+    Position(coordinate_t x, coordinate_t y, Direction d) : _x(x), _y(y), _d(d){}
+
+    friend std::ostream &operator<<(std::ostream &os, const Position &c) {
+        os << "(" << c._x << ", " << c._y << ")" << " " << "TO DO (Direction)";
         return os;
     }
 
 private:
     coordinate_t _x;
     coordinate_t _y;
+    Direction _d;
 };
 
 class Sensor {
@@ -26,56 +31,105 @@ public:
     virtual bool is_safe(coordinate_t x, coordinate_t y) = 0;
 };
 
-enum class Direction {WEST, EAST, SOUTH, NORTH};
-
-class Operation {
-    virtual void run();
-};
-
-class move_forward : public Operation {
+class Command {
 public:
-    move_forward();
+    virtual void run() = 0;
 };
 
-class move_backward : public Operation {
+// TODO
+class MoveForward : public Command {
 public:
-    move_backward();
+    MoveForward() = default;
+
+    void run() override {}
 };
 
-class rotate_right : public Operation {
+// TODO
+class MoveBackward : public Command {
 public:
-    rotate_right();
+    MoveBackward() = default;
+    void run() override {}
 };
 
-class rotate_left : public Operation {
+// TODO
+class RotateRight : public Command {
 public:
-    rotate_left();
+    RotateRight() = default;
+    void run() override {}
 };
 
-class compose : public Operation {
+// TODO
+class RotateLeft : public Command {
 public:
-    compose(std::initializer_list<Operation> list);
+    RotateLeft() = default;
+    void run() override {}
 };
 
+// TODO
+class Compose : public Command {
+public:
+    Compose(std::initializer_list<std::shared_ptr<Command>> list) {
+        for (const auto & it : list) {
+            _commands.push_back(it);
+        }
+    }
+
+    void run() override {}
+
+private:
+    std::vector<std::shared_ptr<Command>> _commands;
+};
+
+std::shared_ptr<MoveForward> move_forward() {
+    return std::make_shared<MoveForward>();
+}
+
+std::shared_ptr<MoveBackward> move_backward() {
+    return std::make_shared<MoveBackward>();
+}
+
+std::shared_ptr<RotateRight> rotate_right() {
+    return std::make_shared<RotateRight>();
+}
+
+std::shared_ptr<RotateLeft> rotate_left() {
+    return std::make_shared<RotateLeft>();
+}
+
+std::shared_ptr<Compose> compose(std::initializer_list<std::shared_ptr<Command>> list) {
+    return std::make_shared<Compose>(list);
+}
+
+//TO DO
 class Rover {
 public:
-    void execute(const std::string &s);
+    Rover() = default;
+    void execute(const std::string &s) {}
 
-    void land(std::pair<coordinate_t, coordinate_t>, Direction d);
+    void land(std::pair<coordinate_t, coordinate_t>, Direction d) {}
 
-    friend std::ostream &operator<<(std::ostream &os, const Rover &r);
+    friend std::ostream &operator<<(std::ostream &os, const Rover &r) {
+        return os;
+    }
 
 };
 
+// TODO
 class RoverBuilder {
 public:
     RoverBuilder() = default;
 
-    RoverBuilder program_command(char, const Operation &o);
+    RoverBuilder& program_command(char, std::shared_ptr<Command> &o) {
+        return *this;
+    }
 
-    RoverBuilder add_sensor(std::unique_ptr<Sensor> s);
+    RoverBuilder& add_sensor(std::unique_ptr<Sensor> s) {
+        return *this;
+    }
 
-    Rover build();
+    Rover build() {
+        return Rover();
+    }
 };
 
 #endif //ROVER_ROVER_H
