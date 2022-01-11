8c83e1446433efd8b2f81c468549beb77a1c6d46
Katarzyna Kloc <kasiakloc00@gmail.com>
2022-01-11 17:59:57 +0100

    Add sensor class

--- Rover/rover.h
+++ Rover/rover.h
@@ -1,4 +1,11 @@
 #ifndef ROVER_ROVER_H
 #define ROVER_ROVER_H
 
+using coordinate_t = int;
+
+class Sensor {
+    virtual bool is_safe([[maybe_unused]] coordinate_t x,
+                         [[maybe_unused]] coordinate_t y) = 0;
+};
+
 #endif //ROVER_ROVER_H
